ba64abc56999a05242a9775cccfdfde4c0d9aaa7
root <siswa.mdluli@gmail.com>
2022-12-12 11:19:33 -0800

    Various sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -a C program that prints the size of various types
+ * Return: 0
+*/
+main(void)
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	return (0);
+}
